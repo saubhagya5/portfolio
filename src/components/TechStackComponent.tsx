30bed9a1cee82ba913235632d23cffbd5ca2fe3a
Shubh Tandon <shubh@Shubhs-MacBook-Air.local>
2025-03-03 20:08:21 +0530

    fixes

--- src/components/TechStackComponent.tsx
+++ src/components/TechStackComponent.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function TechStackComponent({ src, name }) {
+interface TechStackProps {
+  src: string;
+  name: string;
+}
+
+export default function TechStackComponent({ src, name }: TechStackProps) {
   return (
     <div className='h-20 flex flex-col justify-center items-center bg-darkpurple bg-opacity-30 text-sm text-neutral-200 rounded-xl p-6 m-4 '>
-        <div className='hover:translate-x-1 transition duration-200'>
+      <div className='hover:translate-x-1 transition duration-200'>
         <div className='flex-grow flex flex-col items-center rel '>
-        <Image src={src} alt="" width={25} height={25} className='my-2' /></div>
+          <Image src={src} alt="" width={25} height={25} className='my-2' />
+        </div>
         <div className='text-tiny flex-shrink-0'>{name}</div>
-        </div> 
+      </div> 
     </div>
   );
 }
-
