9b2d823d9d1b346f526493217f4033121eb856c7
SaubhagyaTandon <saubhagya.tandon@goorulearning.com>
2025-08-04 23:41:13 +0530

    fixes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,8 +6,8 @@ function Skills() {
     <div className="relative col-span-1 md:col-span-2 bg-black p-4 rounded-3xl group hover:shadow-xl transition duration-200 shadow-input border border-white/[0.2] flex flex-col space-y-4 min-h-fit overflow-hidden">
 <Image src="/bg.png" alt="" layout="fill" objectFit="cover" className="rounded-3xl" />
 
-      <div className="relative z-10 group-hover:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-2xl text-neutral-200 mb-2 mt-2">
+      <div className="group-hover:translate-x-2 transition duration-200">
+        <div className="font-sans font-bold text-xxl text-neutral-200 mb-2 mt-2">
           Skills
         </div>
 
